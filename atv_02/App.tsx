e857684d4c7ce6c1653adfea274c143928c7cafa
Mateus Dalfovo <125157446+MateusDalfovo@users.noreply.github.com>
2023-09-22 12:26:35 -0300

    Update App.tsx

--- atv_02/App.tsx
+++ atv_02/App.tsx
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
+import React = require('react');
 import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  const [FirstNome, setFirstNome] = useState(setFirstNome);
-  const [SurName, setSurName] = useState(setSurName);
-  const [Email, setEmail] = useState(setEmail);
-  const [Senha, setSenha] = useState(setSenha);
-  const [ConfirmarSenha, setConfirmarSenha] = useState(setConfirmarSenha);  
+  const [FirstNome, setFirstNome] = useState('');
+  const [SurName, setSurName] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Senha, setSenha] = useState('');
+  const [ConfirmarSenha, setConfirmarSenha] = useState('');  
   
+  function handleUserAdd() {
+    console.log(
+        "Nome Completo:" + FirstNome
+        + "\nEmail: " + Email
+    )
+}
+
   return (
 
     <ScrollView>
@@ -16,43 +24,53 @@ export default function App() {
       
       <View style={styles.container}>
         <TextInput
-          style={styles.input}
-          onChangeText={setFirstNome}
+          style={styles.input} 
           placeholder="Primeiro Nome"
+
+          onChangeText={setFirstNome}
+          value={FirstNome}
         />
         <Text>Nome: {FirstNome}</Text>
 
         <TextInput
-          style={styles.input}
-          onChangeText={setSurName}
+          style={styles.input}         
           placeholder="Sobrenome"
+
+          value = {SurName}
+          onChangeText={setSurName}
         />
         <Text>Sobrenome: {SurName}</Text>
 
         <TextInput
           style={styles.input}
-          onChangeText={setEmail}
           placeholder="Email"
+
+          value={Email}
+          onChangeText={setEmail}
         />
         <Text>Email: {Email}</Text>
 
         <TextInput
-          style={styles.input}
-          onChangeText={setSenha}
+          style={styles.input}    
           placeholder="Senha"
           secureTextEntry={true}
+          
+          value={Senha}
+          onChangeText={setSenha}
         />
         
 
         <TextInput
-          style={styles.input}
-          onChangeText={setConfirmarSenha}
+          style={styles.input} 
           placeholder="Confirmar Senha"
           secureTextEntry={true}
+
+          value={ConfirmarSenha}
+          onChangeText={setConfirmarSenha}
         />
       
 
-        <TouchableOpacity style={styles.botao}>
+        <TouchableOpacity style={styles.botao} onPress={handleUserAdd}>
           <Text style={styles.textBotao}>Cadastrar</Text>
         </TouchableOpacity>
 
